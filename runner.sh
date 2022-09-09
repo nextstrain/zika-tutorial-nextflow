a07c40e4079557df4b0a44663128d209797fd1a3
j23414 <jenchang@iastate.edu>
2022-09-09 04:57:16 -0700

    refactor: pull original zika config and data files
    This nextflow zika tutorial is based on the nextstrain/zika-tutorial.
    Since there may be updates to the original zika-tutorial such as:
    
    https://github.com/nextstrain/zika-tutorial/pull/8
    
    It seemed more up-to-date, efficient, and streamlined to simply pull the
    default data and config files directly.

--- runner.sh
+++ runner.sh
@@ -1,12 +1,17 @@
 #! /usr/bin/env bash
 
 nextflow run nextstrain/zika-tutorial-nextflow \
- --sequences "data/sequences.fasta" \
- --metadata "data/metadata.tsv" \
- --colors "data/colors.tsv" \
- --auspice_config "data/auspice_config.json" \
- --lat_longs "data/lat_longs.tsv" \
- --colors "data/colors.tsv" \
- --exclude "data/dropped_strains.txt" \
- --reference "data/zika_outgroup.gb" \
  -resume
+
+# Example of passing in your own files
+#  nextflow run nextstrain/zika-tutorial-nextflow \
+#  --sequences "data/sequences.fasta" \
+#  --metadata "data/metadata.tsv" \
+#  --colors "data/colors.tsv" \
+#  --auspice_config "data/auspice_config.json" \
+#  --lat_longs "data/lat_longs.tsv" \
+#  --colors "data/colors.tsv" \
+#  --exclude "data/dropped_strains.txt" \
+#  --reference "data/zika_outgroup.gb" \
+#  -resume
+
